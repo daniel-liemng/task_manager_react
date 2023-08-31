f8be8851156e4fc480654b6f0d03be2e7f5c2f14
Daniel <daniel.liemng@gmail.com>
2023-08-30 22:19:12 -0400

    filter task by category

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import { Task } from './interface/Task';
+import TaskFilter from './components/TaskFilter';
+
+// const taskData = [
+//   { id: 1, title: 'Task 1', dueDate: '09-08-2023', category: 'Work' },
+//   { id: 2, title: 'Task 2', dueDate: '09-08-2023', category: 'Personal' },
+//   { id: 3, title: 'Task 3', dueDate: '09-08-2023', category: 'School' },
+//   { id: 4, title: 'Task 4', dueDate: '09-08-2023', category: 'Work' },
+// ];
 
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>(tasks);
+
+  useEffect(() => {
+    if (searchTerm === 'All') {
+      setFilteredTasks(tasks);
+    } else {
+      const newTasks = tasks.filter((task) =>
+        task.category.includes(searchTerm)
+      );
+      setFilteredTasks(newTasks);
+    }
+  }, [tasks, searchTerm]);
 
   const handleAddTask = (task: Omit<Task, 'id'>, reset: () => void) => {
     const newTask: Task = {
@@ -24,7 +46,12 @@ const App = () => {
     setTasks(newTasks);
   };
 
+  const handleSearchByCategory = (category: string) => {
+    setSearchTerm(category);
+  };
+
   console.log('aaa', tasks);
+  console.log('bbb', searchTerm);
 
   return (
     <div className='w-full h-screen p-3'>
@@ -33,7 +60,8 @@ const App = () => {
           <TaskForm onAddTask={handleAddTask} />
         </div>
         <div className='flex-1'>
-          <TaskList tasks={tasks} onDelete={handleDeleteTask} />
+          <TaskFilter onSearchByCategory={handleSearchByCategory} />
+          <TaskList tasks={filteredTasks} onDelete={handleDeleteTask} />
         </div>
       </div>
     </div>
--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -1,5 +1,30 @@
-const TaskFilter = () => {
-  return <div>TaskFilter</div>;
+import categories from '../categories';
+
+interface TaskFilterProps {
+  onSearchByCategory: (category: string) => void;
+}
+
+const TaskFilter: React.FC<TaskFilterProps> = ({ onSearchByCategory }) => {
+  const allCategories = ['All', ...categories];
+
+  return (
+    <div className='mt-2 mb-5'>
+      <h3 className='text-xl text-orange-500 mb-2'>Search By Category</h3>
+      <select
+        className='p-2 rounded-lg w-full'
+        onChange={(e) => onSearchByCategory(e.target.value)}
+      >
+        <option value='' disabled>
+          Please select one
+        </option>
+        {allCategories.map((category, index) => (
+          <option value={category} key={index}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default TaskFilter;
